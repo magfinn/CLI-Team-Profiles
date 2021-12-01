0e3a859125af51d444351d11dad627e70790907d
wisner.maggiel@gmail.com <wisners@Wisnerss-iMac.local>
2021-12-01 13:33:17 -0800

    converted js to Promises

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
 const generatePage = require('./src/page-template');
+const {writeFile, copyFile}= require('./utils/generate-site.js');
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -126,12 +126,24 @@ const promptProject = portfolioData => {
 };
 
 promptUser()
-.then(promptProject)
-.then(portfolioData => {
-    const pageHTML = generatePage(portfolioData);
-
-fs.writeFile('./dist/index.html'); 
-});
+  .then(promptProject)
+  .then(portfolioData => {
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+ 
 
 
 
